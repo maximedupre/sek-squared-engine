d5c174e3ab193c473c0a4d35a2edcad0cf650a8f
Maxime Dupré <maximedupre@me.com>
2020-11-07 14:56:03 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const POINTS = [
+    [0, 0],
+    [0, 100],
+    [100, 100],
+    [100, 0],
+    [0, 0],
+];
+
+window.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.querySelector('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    draw(canvas, POINTS);
+
+    document.querySelector('input').addEventListener('input', (e) => {
+        const tetha = e.target.value;
+        const newPoints = [];
+
+        for (let point of POINTS) {
+            const x = point[0];
+            let y;
+
+            if (point[1] > 50) {
+                y = (-5 / 9) * tetha + point[1];
+            } else {
+                y = (5 / 9) * tetha + point[1];
+            }
+
+            newPoints.push([x, y]);
+        }
+
+        draw(canvas, newPoints);
+    });
+});
+
+function draw(canvas, points) {
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let point of points) {
+        ctx.lineTo(point[0], point[1]);
+    }
+
+    ctx.stroke();
+}
