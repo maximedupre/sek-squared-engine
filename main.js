13f7e2a83d25be487868169ab4769e4881a4098b
Maxime Dupré <maximedupre@me.com>
2020-11-07 16:35:11 -0500

    doubles sek faces

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-const INITIAL_POINTS = [
-    [0, 0, 0],
-    [0, 100, 0],
-    [100, 100, 0],
-    [100, 0, 0],
-    [0, 0, 0],
+const INITIAL_FACES = [
+    [
+        [0, 0, 0],
+        [0, 100, 0],
+        [100, 100, 0],
+        [100, 0, 0],
+        [0, 0, 0],
+    ],
+    [
+        [0, 0, 0],
+        [0, 0, 100],
+        [100, 0, 100],
+        [100, 0, 0],
+        [0, 0, 0],
+    ],
 ];
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -11,45 +20,57 @@ window.addEventListener('DOMContentLoaded', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    draw(canvas, INITIAL_POINTS);
+    draw(canvas, INITIAL_FACES);
 
     document.querySelector('input').addEventListener('input', (e) => {
         const degrees = e.target.value;
         const radians = degreesToRadians(degrees);
-        const points = [];
+        const faces = [];
 
-        for (let point of INITIAL_POINTS) {
-            const realX = point[0] - 50;
-            const realY = point[1] - 50;
-            const realZ = point[2];
+        for (let face of INITIAL_FACES) {
+            const points = [];
 
-            const x = realX * 1 + realY * 0 + realZ * 0;
-            const y =
-                realX * 0 +
-                realY * Math.cos(radians) +
-                realZ * -Math.sin(radians);
-            const z =
-                realX * 0 +
-                realY * Math.sin(radians) +
-                realZ * Math.cos(radians);
+            for (let point of face) {
+                const realX = point[0] - 50;
+                const realY = point[1] - 50;
+                const realZ = point[2] - 50;
 
-            points.push([x + 50, y + 50, z]);
+                const x = realX * 1 + realY * 0 + realZ * 0;
+                const y =
+                    realX * 0 +
+                    realY * Math.cos(radians) +
+                    realZ * -Math.sin(radians);
+                const z =
+                    realX * 0 +
+                    realY * Math.sin(radians) +
+                    realZ * Math.cos(radians);
+
+                points.push([x + 50, y + 50, z + 50]);
+            }
+
+            faces.push(points);
         }
 
-        draw(canvas, points);
+        draw(canvas, faces);
     });
 });
 
-function draw(canvas, points) {
+function draw(canvas, faces) {
+    const colors = ['blue', 'red'];
     const ctx = canvas.getContext('2d');
-    ctx.beginPath();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let point of points) {
-        ctx.lineTo(point[0], point[1]);
-    }
+    for (let i = 0; i < faces.length; i++) {
+        console.log(colors[i]);
+        ctx.beginPath();
+        ctx.strokeStyle = colors[i];
 
-    ctx.stroke();
+        for (let point of faces[i]) {
+            ctx.lineTo(point[0], point[1]);
+        }
+
+        ctx.stroke();
+    }
 }
 
 function degreesToRadians(degrees) {
