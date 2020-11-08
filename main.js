5766491b410e72f4b4cca4cb3e4203cde88ef24f
Maxime Dupré <maximedupre@me.com>
2020-11-07 19:42:36 -0500

    sek rotation y baileyz LEL

--- main.js
+++ main.js
@@ -84,6 +84,38 @@ window.addEventListener('DOMContentLoaded', () => {
 
         draw(canvas, faces);
     });
+
+    document.querySelector('#slider-y').addEventListener('input', (e) => {
+        const degrees = e.target.value;
+        const faces = [];
+
+        for (let face of INITIAL_FACES) {
+            const points = [];
+
+            for (let point of face.points) {
+                if (point.transition) {
+                    const newPoint = matrixRotationY(point.point, degrees);
+
+                    points.push({
+                        transition: true,
+                        point: newPoint,
+                    });
+                } else {
+                    const newPoint = matrixRotationY(point, degrees);
+
+                    points.push(newPoint);
+                }
+            }
+
+            faces.push({
+                name: face.name,
+                color: face.color,
+                points,
+            });
+        }
+
+        draw(canvas, faces);
+    });
 });
 
 function draw(canvas, faces) {
@@ -140,6 +172,20 @@ function matrixRotationX(point, degrees) {
     const y =
         realX * 0 + realY * Math.cos(radians) + realZ * -Math.sin(radians);
     const z = realX * 0 + realY * Math.sin(radians) + realZ * Math.cos(radians);
+
+    return [x + 50, y + 50, z + 50];
+}
+
+function matrixRotationY(point, degrees) {
+    const radians = degreesToRadians(degrees);
+    const realX = point[0] - 50;
+    const realY = point[1] - 50;
+    const realZ = point[2] - 50;
+
+    const x = realX * Math.cos(radians) + realY * 0 + realZ * Math.sin(radians);
+    const y = realX * 0 + realY * 1 + realZ * 0;
+    const z =
+        realX * -Math.sin(radians) + realY * 0 + realZ * Math.cos(radians);
 
     return [x + 50, y + 50, z + 50];
 }
