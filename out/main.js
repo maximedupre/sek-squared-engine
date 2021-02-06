71820d7b15f9df8892fc4c6edd2cb71ec5c0caeb
Maxime Dupré <maximedupre@me.com>
2021-02-06 18:38:08 -0500

    Updates

--- out/main.js
+++ out/main.js
@@ -15,27 +15,39 @@ window.addEventListener('DOMContentLoaded', function () {
     onSlider(-10, 'x', canvas);
     onSlider(-10, 'y', canvas);
     var TICK = 100;
+    var GRAVITY = 9.81;
     var cumulSecs = 0;
-    var isSpacePressed = false;
-    var acceleration = 0.981;
+    var spaceTicks = 0;
+    var acceleration = 0;
     var speed = 0;
-    setInterval(function () {
-        if (!isSpacePressed) {
-            acceleration = Math.min(acceleration + 0.1, 0.981);
+    var intervalId = setInterval(function () {
+        if (spaceTicks <= 0) {
+            acceleration = Math.min(acceleration + 0.4, GRAVITY);
         }
         else {
-            acceleration = Math.max(acceleration - 0.3, -0.981);
-            isSpacePressed = false;
+            spaceTicks--;
+            acceleration = Math.max(acceleration - 0.5, -GRAVITY);
         }
-        speed += acceleration * cumulSecs;
-        var y = mrua(data.INITIAL_ORIGIN[1], speed, acceleration, cumulSecs);
+        speed += acceleration;
+        var y = mrua(data.INITIAL_ORIGIN[1], speed);
         origin2dTranslation([data.INITIAL_ORIGIN[0], y, 50]);
+        var topThresspassPx = getLimitThresspassPx(canvas, 'top');
+        var bottomThresspassPx = getLimitThresspassPx(canvas, 'bottom');
+        if (topThresspassPx > 0 || bottomThresspassPx < 0) {
+            origin2dTranslation([
+                data.INITIAL_ORIGIN[0],
+                data.INITIAL_ORIGIN[1] +
+                    (topThresspassPx || bottomThresspassPx),
+                50,
+            ]);
+            clearInterval(intervalId);
+        }
         draw(canvas, data.INITIAL_FACES);
         cumulSecs += TICK / 1000;
     }, TICK);
     document.addEventListener('keydown', function (e) {
         if (e.key === ' ') {
-            isSpacePressed = true;
+            spaceTicks = 2;
         }
     });
     document
@@ -104,4 +116,18 @@ function onSlider(value, axis, canvas) {
         data.INITIAL_FACES = faces;
     }
     draw(canvas, faces);
+}
+function getLimitThresspassPx(canvas, type) {
+    var bottom = canvas.height;
+    for (var _i = 0, _a = data.INITIAL_FACES; _i < _a.length; _i++) {
+        var face = _a[_i];
+        for (var _b = 0, _c = face.points; _b < _c.length; _b++) {
+            var point = _c[_b];
+            if ((type === 'bottom' && point[1] > bottom) ||
+                (type === 'top' && point[1] < 0)) {
+                return type === 'bottom' ? bottom - point[1] : -point[1];
+            }
+        }
+    }
+    return 0;
 }
--- out/physics.js
+++ out/physics.js
@@ -1,3 +1,3 @@
-export function mrua(initialPoint, speed, acceleration, cumulSecs) {
-    return initialPoint + speed + 0.5 * acceleration * Math.pow(cumulSecs, 2);
+export function mrua(initialPoint, speed) {
+    return initialPoint + speed;
 }
